d0a163a763a0531426d3173abf30a3789f459285
rb-mwindh <44254831+rb-mwindh@users.noreply.github.com>
2022-11-02 11:18:07 +0100

    test: exclude `.theme.scss` from standard angular rule, prepend rules with `.theme.scss` rule

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,12 +1,8 @@
 module.exports = (config, options, targetOptions) => {
 
-  // log default angular webpack config for debugging
-  RegExp.prototype.toJSON = RegExp.prototype.toString;
-  console.log(JSON.stringify(config, null, 2));
-
   // exclude *.theme.scss files from the angular scss rule
   const scssRule = config.module.rules.find(rule => String(rule.test) === String(scssRegex));
-  scssRule.exclude = [themeRegex];
+  scssRule.test = /\.(?<!theme\.)(?:scss)$/i;
 
   // append new rule for *.theme.scss files
   const themeRule = {
@@ -17,7 +13,11 @@ module.exports = (config, options, targetOptions) => {
       'sass-loader',
     ],
   };
-  config.module.rules.push(themeRule);
+  config.module.rules.splice(0, 0, themeRule);
+
+  // log modified webpack rules for debugging
+  RegExp.prototype.toJSON = RegExp.prototype.toString;
+  console.log(JSON.stringify(config.module.rules, null, 2));
 
   // return the modified webpack config object
   return config;
